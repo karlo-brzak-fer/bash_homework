8f2e4d6e7cedb894aeb009a954ee056f42434798
karlo-brzak-fer <karlo@Karlo.>
2024-11-07 21:00:10 +0100

    hw

---
+++ zad7.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+while [ 1 == 1 ];
+ do
+
+    who | while read -r line; 
+    do
+        usr=$(echo $line | awk '{ print $1 }')
+        date=$(date)
+        usr="$usr $date"
+        echo $usr >> log_history.log
+    done
+sleep $(($1 * 60)) 
+
+done
